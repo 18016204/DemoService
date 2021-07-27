06b321e0a23c110a27d96954b365f5339fc32731
18016204 <57203937+18016204@users.noreply.github.com>
2021-07-27 10:40:39 +0800

    done

--- app/src/main/java/sg/edu/rp/c346/id18016204/demoservice/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id18016204/demoservice/MainActivity.java
@@ -2,13 +2,77 @@ package sg.edu.rp.c346.id18016204.demoservice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnStart ;
+    Button btnstop;
+    Button btnBind;
+    Button btnUnbind;
+
+    private MyService.DownloadBinder downloadBinder;
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            downloadBinder = (MyService.DownloadBinder) iBinder;
+            downloadBinder.startDownload();
+            downloadBinder.getProgress();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnStart=findViewById(R.id.btnStart);
+        btnstop=findViewById(R.id.btnStop);
+        btnBind=findViewById(R.id.btnBind);
+        btnUnbind=findViewById(R.id.btnUnbind);
+
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i  = new Intent(MainActivity.this, MyService.class);
+                startService(i);
+            }
+        });
+        btnstop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i  = new Intent(MainActivity.this, MyService.class);
+                stopService(i);
+            }
+        });
+
+        btnBind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent bindIntent = new Intent(MainActivity.this, MyService.class);
+                bindService(bindIntent, connection, BIND_AUTO_CREATE);
+
+            }
+        });
+
+        btnUnbind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                unbindService(connection);
+            }
+        });
+
     }
 }
